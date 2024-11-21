1b47f4dc9c0aa8447acde791cc2103cd4ca83841
Michael Owuor <michael.owuor@gmail.com>
2024-11-21 12:57:08 +0300

    refactor: modify payload default title suffix

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -24,6 +24,9 @@ export default buildConfig({
 		importMap: {
 			baseDir: path.resolve(dirname),
 		},
+		meta: {
+			titleSuffix: " | Payload",
+		},
 		user: Users.slug,
 	},
 	collections: [Media, Users],
