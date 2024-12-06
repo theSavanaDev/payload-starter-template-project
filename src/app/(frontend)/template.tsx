574a221674774bc94d2cbcdedfcbfc2f8457f9f2
Michael Owuor <michael.owuor@gmail.com>
2024-12-06 09:53:05 +0300

    chore: update dependencies

--- src/app/(frontend)/template.tsx
+++ src/app/(frontend)/template.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from "react";
 
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 const template = ({ children }: { children: ReactNode }) => {
 	return (
